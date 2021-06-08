6dc323be1863d2de5d219f6d88639729d5bfbc49
Chase <cabbott93@gmail.com>
2021-06-08 16:49:04 -0700

    corrected filter function

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@ export default (arr, callback) => {
   const newArray = [];
   for(let i = 0; i < arr.length; i++){
    
-    if((arr[i] !== undefined) && callback(arr[i])) newArray[i] = arr[i]
+    if((arr[i] !== undefined) && callback(arr[i])) newArray[newArray.length] = arr[i]
     ;
   }
   return newArray;
--- filter.test.js
+++ filter.test.js
@@ -2,14 +2,14 @@ import filter from './filter.js';
 
 describe('filter', () => {
   it('filters the array', () => {
-    const array = ['chase', 'sally', 4, 'kevin', true, undefined];
+    const array = ['chase', 'sally', 4, 'kevin', true, undefined, 'chauncey'];
 
     const parameters = (item) => {
       return item[0] === 'c';
     };
 
     const newArray = filter(array, parameters);
-    
-    expect(newArray).toEqual(['chase']);
+
+    expect(newArray).toEqual(['chase', 'chauncey']);
   });
 });
